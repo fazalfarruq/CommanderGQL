bdc8e762f1b1a837b142e6c97dd25843b9dca5b7
Fazal Farruq <FazalF@mmfoodmarket.com>
2021-04-28 19:33:52 -0400

    Add project files.

---
+++ GraphQL/Query.cs
@@ -0,0 +1,24 @@
+using System.Linq;
+using CommanderGQL.Models;
+using HotChocolate;
+using HotChocolate.Data;
+
+namespace CommanderGQL.GraphQL
+{
+    public class Query
+    {
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        public IQueryable<Platform> GetPlatform([ScopedService] AppDbContext context)
+        {
+            return context.Platforms;
+        }
+
+        [UseDbContext(typeof(AppDbContext))]
+        [UseProjection]
+        public IQueryable<Command> GetCommand([ScopedService] AppDbContext context)
+        {
+            return context.Commands;
+        }
+    }
+}
